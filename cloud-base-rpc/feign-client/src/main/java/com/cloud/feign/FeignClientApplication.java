c498c0e7cf9700a735464e16adda02627b362fb0
xieyong <xieyong@startdt.com>
2018-10-25 14:19:30 +0800

    feign消费

--- cloud-base-rpc/feign-client/src/main/java/com/cloud/feign/FeignClientApplication.java
+++ cloud-base-rpc/feign-client/src/main/java/com/cloud/feign/FeignClientApplication.java
@@ -2,11 +2,15 @@ package com.cloud.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
  * @author xieyong
  */
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class FeignClientApplication {
 
     public static void main(String[] args) {
--- cloud-base-rpc/feign-client/src/main/java/com/cloud/feign/interfaces/AuthFeignClient.java
+++ cloud-base-rpc/feign-client/src/main/java/com/cloud/feign/interfaces/AuthFeignClient.java
@@ -1,10 +1,20 @@
 package com.cloud.feign.interfaces;
 
+import com.cloud.feign.entity.User;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * @author: xieyong
  * @date: 2018/9/4 19:33
  * @Description:
  */
+@FeignClient("eureka-server-1")
 public interface AuthFeignClient {
 
+    @GetMapping(value = "/plus/testPage",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    Object getuserinfo();
 }
