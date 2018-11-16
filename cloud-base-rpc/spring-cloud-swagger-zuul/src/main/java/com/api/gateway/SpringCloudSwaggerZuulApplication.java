4a75438fc20745c493721eb61b721582c757f09d
xieyong <xieyong@startdt.com>
2018-11-16 17:57:02 +0800

    数据监控-turbine加入

--- cloud-base-rpc/spring-cloud-swagger-zuul/src/main/java/com/api/gateway/SpringCloudSwaggerZuulApplication.java
+++ cloud-base-rpc/spring-cloud-swagger-zuul/src/main/java/com/api/gateway/SpringCloudSwaggerZuulApplication.java
@@ -4,6 +4,8 @@ import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
@@ -15,9 +17,14 @@ import java.util.List;
 
 /**
  * Zuul默认已经整合了Hystrix
+ * 数据监控: http://localhost:8085/hystrix/monitor
  */
 @EnableSwagger2Doc
 @EnableZuulProxy
+//启用Turbine
+@EnableTurbine
+//启用Dashboard
+@EnableHystrixDashboard
 @SpringCloudApplication
 public class SpringCloudSwaggerZuulApplication {
 
